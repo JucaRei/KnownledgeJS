ba325920adad0c34298e9bf19dbc58de394d1b7e
Juca <reinaldo800@gmail.com>
2019-10-19 19:32:44 -0300

    Pequenas mudanças

--- frontend/src/components/admin/ArticleAdmin.vue
+++ frontend/src/components/admin/ArticleAdmin.vue
@@ -1,15 +1,122 @@
 <template>
   <div class="article-admin">
-      <h1>Artigo Componente</h1>
+    <b-form>
+      <input id="category-id" type="hidden" v-model="article.id" />
+      <b-form-group label="Nome:" label-for="category-name">
+        <b-form-input
+          id="category-name"
+          type="text"
+          v-model="article.name"
+          required
+          :readonly="mode === 'remove'"
+          placeholder="Informe o Nome da Categoria..."
+        />
+      </b-form-group>
+      <!-- Combo box, aponta para a categoria Pai daquele elemento -->
+      <b-form-group label="Categoria Pai:" label-for="category-parentId">
+        <b-form-select v-if="mode === 'save'"
+          label
+          id="category-parentId"
+          :options="categories"
+          v-model="article.parentId"
+        />
+      </b-form-group>
+      <b-form-group label="Categoria Pai:" label-for="category-parentId">
+
+      </b-form-group>
+      <b-button variant="primary" v-if="mode === 'save'" @click="save">Salvar</b-button>
+      <b-button variant="danger" v-if="mode === 'remove'" @click="remove">Excluir</b-button>
+      <b-button class="ml-2" @click="reset">Cancelar</b-button>
+    </b-form>
+    <hr />
+    <b-table hover striped :items="categories" :fields="fields">
+      <template slot="actions" slot-scope="data">
+        <b-button variant="warning" @click="loadCategory(data.item)" class="mr-2">
+          <i class="fa fa-pencil"></i>
+        </b-button>
+        <b-button variant="danger" @click="loadCategory(data.item, 'remove')">
+          <i class="fa fa-trash"></i>
+        </b-button>
+      </template>
+    </b-table>
   </div>
 </template>
 
 <script>
+import { VueEditor } from 'vue2-editor'
+import { baseApiUrl, showError } from "@/global";
+import axios from "axios";
+
 export default {
-    name: 'ArticleAdmin'
-}
+  name: "ArticleAdmin",
+  // registrar o componente
+  componentes: { VueEditor },
+  data: function() {
+    return {
+      mode: "save",
+      article: {},
+      articles: [],
+      categories: [],   // precisa ter categorias (comboBox) e usuarios
+      users: [],
+      // consulta paginada
+      page: 1,
+      limit: 0,
+      count: 0, //paginador
+      fields: [
+        { key: "id", label: "Código", sortable: true },
+        { key: "name", label: "Nome", sortable: true },
+        { key: "path", label: "Caminho", sortable: true },
+        { key: "actions", label: "Ações" }
+      ]
+    };
+  },
+  methods: {
+    loadCategories() {
+      const url = `${baseApiUrl}/categories`;
+      axios.get(url).then(res => {
+        // this.categories = res.data
+        // Combo box - usar os atributos padrão
+        this.categories = res.data.map(category => {
+          return { ...category, value: category.id, text: category.path };
+          // path coloca no texto do combo box, e o value seleciona o id quando usar o combo box(mostra com o caminho correto)
+        });
+      });
+    },
+    reset() {
+      this.mode = "save";
+      this.category = {};
+      this.loadCategories();
+    },
+    save() {
+      const method = this.category.id ? "put" : "post";
+      const id = this.category.id ? `/${this.category.id}` : "";
+      axios[method](`${baseApiUrl}/categories${id}`, this.category)
+        .then(() => {
+          this.$toasted.global.defaultSuccess();
+          this.reset();
+        })
+        .catch(showError);
+    },
+    remove() {
+      const id = this.category.id;
+      axios
+        .delete(`${baseApiUrl}/categories/${id}`)
+        .then(() => {
+          this.$toasted.global.defaultSuccess();
+          this.reset();
+        })
+        .catch(showError);
+    },
+    loadCategory(category, mode = "save") {
+      this.mode = mode;
+      this.category = { ...category };
+    }
+  },
+  mounted() {
+    this.loadCategories();
+  }
+};
 </script>
 
 <style>
-
 </style>
--- frontend/src/components/admin/UserAdmin.vue
+++ frontend/src/components/admin/UserAdmin.vue
@@ -1,96 +1,84 @@
 <template>
-  <div class="user-admin">
-    <b-form>
-      <!-- se o usuario estiver setado -->
-      <input id="user-id" type="hidden" v-model="user.id" />
-      <!-- linha do formulário -->
-      <b-row>
-        <!-- essa linha vai ocupar 6 colunas nos dispositivos médios e 12 nos dispositivos pequenos( disp moveis) -->
-        <b-col md="6" sm="12">
-          <!-- readonly se for para o modo exclusão -->
-          <b-form-group label="Nome:" label-for="user-name">
-            <b-form-input
-              id="user-name"
-              type="text"
-              v-model="user.name"
-              required
-              :readonly="mode === 'remove'"
-              placeholder="Informe o Nome do Usuário"
-            ></b-form-input>
-          </b-form-group>
-        </b-col>
-        <!-- essa linha vai ocupar 6 colunas nos dispositivos médios e 12 nos dispositivos pequenos( disp moveis) -->
-        <b-col md="6" sm="12">
-          <b-form-group label="E-mail:" label-for="user-email">
-            <b-form-input
-              id="user-email"
-              type="text"
-              v-model="user.email"
-              required
-              :readonly="mode === 'remove'"
-              placeholder="Informe o E-mail do Usuário"
-            ></b-form-input>
-          </b-form-group>
-        </b-col>
-      </b-row>
-      <!-- mt3 (margin top 3) bt3 (margin bottom 3) -->
-      <b-form-checkbox id="user-admin" v-model="user.admin"
-        v-show="mode === save" class="mt-3 mb-3">Administrador ?</b-form-checkbox>
-      <!-- linha do formulário -->
-      <b-row v-show="mode === 'save'">
-        <b-col md="6" sm="12">
-          <b-form-group label="Senha:" label-for="user-password">
-            <b-form-input
-              id="user-password"
-              type="password"
-              v-model="user.password"
-              required
-              placeholder="Informe a Senha do Usuário..."
-            />
-          </b-form-group>
-        </b-col>
-        <!-- essa linha vai ocupar 6 colunas nos dispositivos médios e 12 nos dispositivos pequenos( disp moveis) -->
-        <b-col md="6" sm="12">
-          <b-form-group label="Confirmação de Senha:" label-for="user-confirm-password">
-            <b-form-input
-              id="user-confirm-password"
-              type="password"
-              v-model="user.confirmPassword"
-              required
-              placeholder="Confirme a Senha do Usuário..."
-            />
-          </b-form-group>
-        </b-col>
-      </b-row>
-      <!-- botão vai aparecer em suas próprias linhas -->
-      <b-row>
-        <!-- qualquer dispositivo vai ocupar 12 colunas -->
-        <b-col xs="12">
-          <!-- botões de ação(salvar, excluir, etc) -->
-          <!-- esse botão vai apenas estar disponivel, quando o modo for save -->
-          <b-button variant="primary" v-if="mode === 'save'" @click="save">Salvar</b-button>
-          <!-- Excluir, so aparece SE estiver com o modo remove -->
-          <b-button variant="danger" v-if="mode === 'remove'" @click="remove">Excluir</b-button>
-          <!-- Botão de resetar o formulário, (ml-2 - margin left 2) -->
-          <b-button class="ml-2" @click="reset">Cancelar</b-button>
-        </b-col>
-      </b-row>
-    </b-form>
-    <hr />
-    <!-- hover com tabela zebrada(striped) -->
-    <b-table hover striped :items="users" :fields="fields">
-      <!-- relacionado aos actions -->
-      <template slot="actions" slot-scope="data">
-        <!-- botões para mudar ou excluir usuario -->
-        <b-button variant="warning" @click="loadUser(data.item)" class="mr-2">
-          <i class="fa fa-pencil"></i>
-        </b-button>
-        <b-button variant="danger" @click="loadUser(data.item, 'remove')">
-          <i class="fa fa-trash"></i>
-        </b-button>
-      </template>
-    </b-table>
-  </div>
+    <div class="user-admin">
+        <b-form>
+          <!-- se o usuario estiver setado -->
+            <input id="user-id" type="hidden" v-model="user.id" />
+            <!-- linha do formulário -->
+            <b-row>
+              <!-- essa linha vai ocupar 6 colunas nos dispositivos médios e 12 nos dispositivos pequenos( disp moveis) -->
+                <b-col md="6" sm="12">
+                  <!-- readonly se for para o modo exclusão -->
+                    <b-form-group label="Nome:" label-for="user-name">
+                        <b-form-input id="user-name" type="text"
+                            v-model="user.name" required
+                            :readonly="mode === 'remove'"
+                            placeholder="Informe o Nome do Usuário..." />
+                    </b-form-group>
+                </b-col>
+                <!-- essa linha vai ocupar 6 colunas nos dispositivos médios e 12 nos dispositivos pequenos( disp moveis) -->
+                <b-col md="6" sm="12">
+                    <b-form-group label="E-mail:" label-for="user-email">
+                        <b-form-input id="user-email" type="text"
+                            v-model="user.email" required
+                            :readonly="mode === 'remove'"
+                            placeholder="Informe o E-mail do Usuário..." />
+                    </b-form-group>
+                </b-col>
+            </b-row>
+            <!-- mt3 (margin top 3) bt3 (margin bottom 3) -->
+            <!-- linha do formulário -->
+            <b-form-checkbox id="user-admin" v-show="mode === 'save'"
+                v-model="user.admin" class="mt-3 mb-3">
+                Administrador?
+            </b-form-checkbox>
+            <b-row v-show="mode === 'save'">
+                <b-col md="6" sm="12">
+                    <b-form-group label="Senha:" label-for="user-password">
+                        <b-form-input id="user-password" type="password"
+                            v-model="user.password" required
+                            placeholder="Informe a Senha do Usuário..." />
+                    </b-form-group>
+                </b-col>
+                <!-- essa linha vai ocupar 6 colunas nos dispositivos médios e 12 nos dispositivos pequenos( disp moveis) -->
+                <b-col md="6" sm="12">
+                    <b-form-group label="Confirmação de Senha:"
+                        label-for="user-confirm-password">
+                        <b-form-input id="user-confirm-password" type="password"
+                            v-model="user.confirmPassword" required
+                            placeholder="Confirme a Senha do Usuário..." />
+                    </b-form-group>
+                </b-col>
+            </b-row>
+            <!-- botão vai aparecer em suas próprias linhas -->
+            <b-row>
+                <b-col xs="12">
+                    <!-- botões de ação(salvar, excluir, etc) -->
+                    <!-- esse botão vai apenas estar disponivel, quando o modo for save -->
+                    <b-button variant="primary" v-if="mode === 'save'"
+                        @click="save">Salvar</b-button>
+                    <!-- Excluir, so aparece SE estiver com o modo remove -->
+                    <b-button variant="danger" v-if="mode === 'remove'"
+                        @click="remove">Excluir</b-button>
+                        <!-- Botão de resetar o formulário, (ml-2 - margin left 2) -->
+                    <b-button class="ml-2" @click="reset">Cancelar</b-button>
+                </b-col>
+            </b-row>
+        </b-form>
+        <hr>
+        <!-- hover com tabela zebrada(striped) -->
+        <b-table hover striped :items="users" :fields="fields">
+          <!-- relacionado aos actions -->
+            <template slot="actions" slot-scope="data">
+              <!-- botões para mudar ou excluir usuario -->
+                <b-button variant="warning" @click="loadUser(data.item)" class="mr-2">
+                    <i class="fa fa-pencil"></i>
+                </b-button>
+                <b-button variant="danger" @click="loadUser(data.item, 'remove')">
+                    <i class="fa fa-trash"></i>
+                </b-button>
+            </template>
+        </b-table>
+    </div>
 </template>
 
 <script>
